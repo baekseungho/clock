bfdf3186e588f82c33d0d3a29b3c124ccca7adc6
baekseungho <vudrkd1580@naver.com>
2023-02-07 16:16:04 +0900

    중간점검

---
+++ js/script.js
@@ -0,0 +1,50 @@
+var clock = document.querySelector('#time');
+
+function getTime(){
+  const time = new Date();
+  const hour = time.getHours();
+  const minutes = time.getMinutes();
+  const seconds = time.getSeconds();
+  clock.innerHTML = `${hour<10 ? `0${hour}`:hour} : ${minutes<10 ? `0${minutes}`:minutes} : ${seconds<10 ? `0${seconds}`:seconds}`
+}
+
+function init(){
+  setInterval(getTime, 1000);
+}
+
+init();
+
+$('.fnb-list .inputbox').click(function(e){
+  e.preventDefault();
+  $('.fnb-list .inputbox').removeClass('btn-primary');
+  $('.fnb-list .inputbox').removeAttr('checked');
+
+  $(this).addClass('btn-primary');
+  $(this).children('input[type="radio"]').attr('checked', 'checked');
+  if($(this).children('input[type="radio"]').val()==0){
+    $('.back').css({"background-image":"url(./img/bg_morning.jpg)"});
+    $('.clock-box').css({"background-image":"url(./img/phone_morning.png)"});
+    $('.logo img').attr("src","./img/logo_b.png")
+    $('header a').css("color","#000");
+    $('main').css("color","#000");
+  }else if($(this).children('input[type="radio"]').val()==1){
+    $('.back').css({"background-image":"url(./img/bg_afternoon.jpg)"});
+    $('.clock-box').css({"background-image":"url(./img/phone_afternoon.png)"});
+    $('.logo img').attr("src","./img/logo_b.png")
+    $('header a').css("color","#000");
+    $('main').css("color","#000");
+  }else if($(this).children('input[type="radio"]').val()==2){
+    $('.back').css({"background-image":"url(./img/bg_evening.jpg)"});
+    $('.clock-box').css({"background-image":"url(./img/phone_evening.png)"});
+    $('.logo img').attr("src","./img/logo_b.png")
+    $('header a').css("color","#000");
+    $('main').css("color","#000");
+  }else if($(this).children('input[type="radio"]').val()==3){
+    $('.back').css({"background-image":"url(./img/bg_night.jpg)"});
+    $('.clock-box').css({"background-image":"url(./img/phone_night.png)"});
+    $('.logo img').attr("src","./img/logo_w.png");
+    $('header a').css("color","#fff");
+    $('main').css("color", "#fff");
+  }
+});
+
